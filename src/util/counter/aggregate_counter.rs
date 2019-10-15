381376a22b244776254000fc73c627b7fcbfe72f
DanielMorton <dacmorton@gmail.com>
2019-10-15 10:54:10 -0400

    Add tests

--- src/util/counter/aggregate_counter.rs
+++ src/util/counter/aggregate_counter.rs
@@ -47,16 +47,23 @@ mod tests {
     use super::AggregateCounter;
     use super::super::Counter;
 
+    lazy_static! {
+        static ref NUMS_VEC: Vec<i32> = vec![45, 5, 52, 93, 51, 90];
+        static ref DEC_VEC: Vec<u32> = vec![79, 42, 11, 29, 76];
+        static ref FLOAT_VEC: Vec<f64> = vec![9.62, 4.0, 4.6, 7.73, 7.59];
+    }
+
     #[test]
     fn test_aggregate_counter() {
-        let mut ac: AggregateCounter<u32> = AggregateCounter::new();
-        for i in 1..=6 {
-            ac.update(i)
+        let mut ac: AggregateCounter<i32> = AggregateCounter::new();
+        for i in 0..=5 {
+            ac.update(NUMS_VEC[i])
         }
         assert_eq!(*ac.counter(), 6);
-        assert_eq!(*ac.total(), 21);
-        assert_eq!(ac.average(), 3.5);
+        assert_eq!(*ac.total(), 336);
+        assert_eq!(ac.average(), 56.0);
         ac.reset();
+
         assert_eq!(*ac.counter(), 0);
         for _ in 1..=7 {
             ac.update(3)
@@ -64,5 +71,31 @@ mod tests {
         assert_eq!(*ac.counter(), 7);
         assert_eq!(*ac.total(), 21);
         assert_eq!(ac.average(), 3.0);
+    }
+
+    #[test]
+    fn test_aggregate_int_counter() {
+        let mut ac: AggregateCounter<u32> = AggregateCounter::new();
+        for i in 0..=4 {
+            ac.update(DEC_VEC[i])
+        }
+        assert_eq!(*ac.counter(), 5);
+        assert_eq!(*ac.total(), 237);
+        assert_eq!(ac.average(), 47.4);
+        ac.reset();
+        assert_eq!(*ac.counter(), 0);
+    }
+
+    #[test]
+    fn test_aggregate_float_counter() {
+        let mut ac: AggregateCounter<f64> = AggregateCounter::new();
+        for i in 0..=4 {
+            ac.update(FLOAT_VEC[i])
+        }
+        assert_eq!(*ac.counter(), 5.0);
+        assert_eq!(*ac.total(), 33.54);
+        assert_eq!(ac.average(), 6.708);
+        ac.reset();
+        assert_eq!(*ac.counter(), 0.0);
     }
 }
--- src/util/counter/base_counter.rs
+++ src/util/counter/base_counter.rs
@@ -37,6 +37,12 @@ mod tests {
     use super::BaseCounter;
     use super::super::Counter;
 
+    lazy_static! {
+        static ref POS_VEC: Vec<u32> = vec![3, 7, 9, 8, 4, 10, 5];
+        static ref NUMS_VEC: Vec<i32> = vec![10, -8, 0, -4, 8, 9, -7];
+        static ref FLOAT_VEC: Vec<f64> = vec![8.44, 4.93, -3.67, 8.91, -4.74, -1.67, 6.26];
+    }
+
     #[test]
     fn test_base_counter() {
         let mut bc: BaseCounter<u32> = BaseCounter::new();
@@ -46,11 +52,33 @@ mod tests {
         assert_eq!(*bc.counter(), 6);
         bc.reset();
         assert_eq!(*bc.counter(), 0);
-        for i in 1..=6 {
-            bc.update(i)
+        for i in 0..=6 {
+            bc.update(POS_VEC[i])
         }
-        assert_eq!(*bc.counter(), 21);
+        assert_eq!(*bc.counter(), 46);
         bc.reset();
         assert_eq!(*bc.counter(), 0)
+    }
+
+    #[test]
+    fn test_base_int_counter() {
+        let mut bc: BaseCounter<i32> = BaseCounter::new();
+        for i in 0..=6 {
+            bc.update(NUMS_VEC[i])
+        }
+        assert_eq!(*bc.counter(), 8);
+        bc.reset();
+        assert_eq!(*bc.counter(), 0)
+    }
+
+    #[test]
+    fn test_base_float_counter() {
+        let mut bc: BaseCounter<f64> = BaseCounter::new();
+        for i in 0..=6 {
+            bc.update(FLOAT_VEC[i])
+        }
+        assert_eq!(*bc.counter(), 18.46);
+        bc.reset();
+        assert_eq!(*bc.counter(), 0.0)
     }
 }
