bc99a48eb1ad6ff982895cd0e16ffff7c2cac392
DanielMorton <dacmorton@gmail.com>
2019-10-16 13:18:24 -0400

    add multithread and gitignore

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+extern crate scoped_threadpool;
+
 use std::fs::File;
 use std::io::Write;
 use std::time::Instant;
@@ -5,11 +7,12 @@ use std::time::Instant;
 use rand::distributions::Distribution;
 use rand::thread_rng;
 use rand_distr::uniform::Uniform;
+use scoped_threadpool::Pool;
 
-use ratel::{Agent, BinomialBandit, Game, GreedyAgent, HarmonicStepper};
+use ratel::{BinomialBandit, Game, GreedyAgent, HarmonicStepper};
 
 fn greedy_bernoulli(runs: u32, iterations: u32, agent_start: f64) {
-    let rewards = vec![0.1, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19];
+    let rewards = vec![0.1, 0.11, 0.95, 0.13, 0.94, 0.15, 0.16, 0.17, 0.18, 0.19];
 
     let mut stepper = HarmonicStepper::new(1, rewards.len());
     let rand_start = Uniform::new(agent_start - 1e-7, agent_start + 1e-7);
@@ -22,7 +25,6 @@ fn greedy_bernoulli(runs: u32, iterations: u32, agent_start: f64) {
     let mut wins = vec![0.0; iterations as usize];
     let mut reward_out = vec![0.0; iterations as usize];
 
-    let start = Instant::now();
     for _ in 0..=runs {
         game.run(iterations);
         wins = wins.into_iter().zip(game.wins().into_iter())
@@ -36,15 +38,23 @@ fn greedy_bernoulli(runs: u32, iterations: u32, agent_start: f64) {
     reward_out = reward_out.into_iter().map(|ro| ro / f64::from(runs)).collect();
     let greedy = wins.into_iter().zip(reward_out.into_iter())
         .map(|x| format!("{}, {}", x.0, x.1))
-        .fold(String::from("wins, rewards"), |s, x| [s, x].join("\n"));
-    let mut file = File::create(format!("greedy_{}.csv", agent_start)).unwrap();
-    file.write_all(greedy.as_bytes());
-
-    println!("{}", start.elapsed().as_secs());
+        .fold(String::from("wins,rewards"), |s, x| [s, x].join("\n"));
+    let mut file = File::create(format!("greedy/greedy_{}.csv", agent_start)).unwrap();
+    file.write_all(greedy.as_bytes()).unwrap();
 }
 
 fn main() {
     let n = 100u32;
     let r = 1000000u32;
-    (1..=10).into_iter().for_each(|x| greedy_bernoulli(r, n, f64::from(x) / 10.0))
+    let mut pool = Pool::new(12);
+    let vec: Vec<u32> = (1..=100).into_iter().map(|x| x).collect();
+    let start = Instant::now();
+    pool.scoped(|scope| {
+        for x in vec {
+            scope.execute(move || {
+                greedy_bernoulli(r, n, f64::from(x) / 100.0)
+            })
+        }
+    });
+    println!("{}", start.elapsed().as_secs());
 }
