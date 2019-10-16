dc437f1dee3338aacd42a49b5b187bef2e9419a8
DanielMorton <dacmorton@gmail.com>
2019-10-16 13:19:51 -0400

    fmt

--- src/main.rs
+++ src/main.rs
@@ -16,10 +16,13 @@ fn greedy_bernoulli(runs: u32, iterations: u32, agent_start: f64) {
 
     let mut stepper = HarmonicStepper::new(1, rewards.len());
     let rand_start = Uniform::new(agent_start - 1e-7, agent_start + 1e-7);
-    let mut agent = GreedyAgent::new((1..=rewards.len()).into_iter()
-                                         .map(|_| rand_start.sample(&mut thread_rng()))
-                                         .collect(),
-                                     &mut stepper);
+    let mut agent = GreedyAgent::new(
+        (1..=rewards.len())
+            .into_iter()
+            .map(|_| rand_start.sample(&mut thread_rng()))
+            .collect(),
+        &mut stepper,
+    );
     let bandit = BinomialBandit::new(vec![1; rewards.len()], rewards.clone());
     let mut game = Game::new(&mut agent, &bandit);
     let mut wins = vec![0.0; iterations as usize];
@@ -27,16 +30,31 @@ fn greedy_bernoulli(runs: u32, iterations: u32, agent_start: f64) {
 
     for _ in 0..=runs {
         game.run(iterations);
-        wins = wins.into_iter().zip(game.wins().into_iter())
-            .map(|w| w.0 + f64::from(*w.1)).collect();
-        reward_out = reward_out.into_iter().zip(game.rewards().into_iter())
-            .map(|ro| ro.0 + f64::from(*ro.1)).collect();
-        game.reset((1..=rewards.len()).into_iter()
-            .map(|_| rand_start.sample(&mut thread_rng())).collect())
+        wins = wins
+            .into_iter()
+            .zip(game.wins().into_iter())
+            .map(|w| w.0 + f64::from(*w.1))
+            .collect();
+        reward_out = reward_out
+            .into_iter()
+            .zip(game.rewards().into_iter())
+            .map(|ro| ro.0 + f64::from(*ro.1))
+            .collect();
+        game.reset(
+            (1..=rewards.len())
+                .into_iter()
+                .map(|_| rand_start.sample(&mut thread_rng()))
+                .collect(),
+        )
     }
     wins = wins.into_iter().map(|w| w / f64::from(runs)).collect();
-    reward_out = reward_out.into_iter().map(|ro| ro / f64::from(runs)).collect();
-    let greedy = wins.into_iter().zip(reward_out.into_iter())
+    reward_out = reward_out
+        .into_iter()
+        .map(|ro| ro / f64::from(runs))
+        .collect();
+    let greedy = wins
+        .into_iter()
+        .zip(reward_out.into_iter())
         .map(|x| format!("{}, {}", x.0, x.1))
         .fold(String::from("wins,rewards"), |s, x| [s, x].join("\n"));
     let mut file = File::create(format!("greedy/greedy_{}.csv", agent_start)).unwrap();
@@ -51,9 +69,7 @@ fn main() {
     let start = Instant::now();
     pool.scoped(|scope| {
         for x in vec {
-            scope.execute(move || {
-                greedy_bernoulli(r, n, f64::from(x) / 100.0)
-            })
+            scope.execute(move || greedy_bernoulli(r, n, f64::from(x) / 100.0))
         }
     });
     println!("{}", start.elapsed().as_secs());
