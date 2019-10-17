1bedcac6ac27888add60cb734abf4e81c9a143d7
DanielMorton <dacmorton@gmail.com>
2019-10-17 14:07:27 -0400

    command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,75 @@
-extern crate scoped_threadpool;
-
 use std::time::Instant;
 
+use clap::{App, Arg, SubCommand, value_t};
 use scoped_threadpool::Pool;
 
 use ratel::{epsilon_bernoulli, greedy_bernoulli};
 
 fn main() {
-    let n = 10000u32;
-    let r = 10000u32;
+    let matches = App::new("Ratel")
+        .arg(
+            Arg::with_name("runs")
+                .short("r")
+                .long("runs")
+                .help("Number of trial runs.")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("iterations")
+                .short("n")
+                .help("Number of iterations in each trial")
+                .required(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("greedy")
+                .short("g")
+                .long("greedy")
+                .help("Use the greedy algorithm."),
+        )
+        .arg(
+            Arg::with_name("epsilon_greedy")
+                .short("e")
+                .long("epsilon")
+                .help("Use the epsilon-greedy algorithm")
+                .takes_value(true)
+                .conflicts_with("greedy"),
+        )
+        .get_matches();
+    let runs = value_t!(matches.value_of("runs"), u32).unwrap_or_else(|e| e.exit());
+    let iterations = value_t!(matches.value_of("iterations"), u32).unwrap_or_else(|e| e.exit());
+    if matches.is_present("greedy") {
+        run_greedy(runs, iterations)
+    } else if matches.is_present("epsilon_greedy") {
+        let epsilon =
+            value_t!(matches.value_of("epsilon_greedy"), f64).unwrap_or_else(|e| e.exit());
+        run_epsilon(runs, iterations, epsilon)
+    }
+}
+
+fn run_epsilon(runs: u32, iterations: u32, epsilon: f64) {
     let mut pool = Pool::new(12);
     let vec: Vec<u32> = (1..=100).into_iter().map(|x| x).collect();
     let start = Instant::now();
     pool.scoped(|scope| {
         for x in vec {
-            scope.execute(move || epsilon_bernoulli(r, n, f64::from(x) / 100.0, 0.01))
+            scope.execute(move || {
+                epsilon_bernoulli(runs, iterations, f64::from(x) / 100.0, epsilon)
+            });
+            // scope.execute(move || epsilon_bernoulli(r, n, f64::from(x) / 100.0, 0.1))
+        }
+    });
+    println!("{}", start.elapsed().as_secs());
+}
+
+fn run_greedy(runs: u32, iterations: u32) {
+    let mut pool = Pool::new(12);
+    let vec: Vec<u32> = (1..=100).into_iter().map(|x| x).collect();
+    let start = Instant::now();
+    pool.scoped(|scope| {
+        for x in vec {
+            scope.execute(move || greedy_bernoulli(runs, iterations, f64::from(x) / 100.0));
+            // scope.execute(move || epsilon_bernoulli(r, n, f64::from(x) / 100.0, 0.1))
         }
     });
     println!("{}", start.elapsed().as_secs());
