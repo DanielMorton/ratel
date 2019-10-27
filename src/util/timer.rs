315f17e5a76d07a9b146ad6af072906a16014695
DanielMorton <dacmorton@gmail.com>
2019-10-27 12:31:43 -0400

    Make timer output one line

--- src/util/timer.rs
+++ src/util/timer.rs
@@ -3,5 +3,5 @@ use std::time::Instant;
 pub fn print_hms(start: Instant) -> () {
     let seconds = start.elapsed().as_secs();
     let (hour, minute, second) = (seconds / 3600, (seconds % 3600) / 60, seconds % 60);
-    print!("{}:{}:{}", hour, minute, second)
+    println!("{}:{}:{}", hour, minute, second)
 }
