23ef01d0d3b5f167eb1fa43a99f5d7e34d25edde
DanielMorton <dacmorton@gmail.com>
2019-10-27 20:55:12 -0400

    Fix timer formatting

--- src/util/timer.rs
+++ src/util/timer.rs
@@ -3,5 +3,6 @@ use std::time::Instant;
 pub fn print_hms(start: Instant) -> () {
     let seconds = start.elapsed().as_secs();
     let (hour, minute, second) = (seconds / 3600, (seconds % 3600) / 60, seconds % 60);
-    println!("{}:{}:{}", hour, minute, second)
+    println!("{}:{}:{}", hour, if minute < 10 { format!("0{}", minute) } else { format!("{}", minute) },
+             if second < 10 { format!("0{}", seconds) } else { format!("{}", second) })
 }
