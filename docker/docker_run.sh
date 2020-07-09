5084943b841c497c3c98bcf81e2dd6f6b1b03984
DanielMorton <dacmorton@gmail.com>
2020-07-09 10:14:33 -0400

    docker

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,37 +1,36 @@
 #!/usr/bin/env bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 PARENTDIR=$(cd ../ && pwd)
 
 source ${DIR}/docker_env.sh
 
-while :;
-do
-    case $1 in
-        -r|--runs)
-            RUNS="$2"
-            shift
-            ;;
-        -n|--num)
-            NUM_ITERS="$2"
-            shift
-            ;;
-        -e|--epsilon)
-            TYPE="-e $2"
-            shift
-            ;;
-        -g|--greedy)
-            TYPE="-g"
-            shift
-            ;;
-        -o|--optimistic)
-            TYPE="--pair-optimistic $2"
-            shift
-            ;;
-        *)  break
-    esac
-
+while :; do
+  case $1 in
+  -r | --runs)
+    RUNS="$2"
     shift
+    ;;
+  -n | --num)
+    NUM_ITERS="$2"
+    shift
+    ;;
+  -e | --epsilon)
+    TYPE="-e $2"
+    shift
+    ;;
+  -g | --greedy)
+    TYPE="-g"
+    shift
+    ;;
+  -o | --optimistic)
+    TYPE="--pair-optimistic $2"
+    shift
+    ;;
+  *) break ;;
+  esac
+
+  shift
 done
 
 docker run ${DOCKER_IMAGE_NAME} -n $NUM_ITERS -r $RUNS $TYPE
